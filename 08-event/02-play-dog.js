82330dabc82e3a66595320075173bf275a5effc8
zhangshuo <443494891@qq.com>
2019-09-11 18:32:43 +0800

    add

--- 08-event/02-play-dog.js
+++ 08-event/02-play-dog.js
@@ -8,6 +8,6 @@ var zangao = new Dog('zangao',10);
 taidi.on('bark',onBark);
 zangao.on('bark',onBark);
 
-taidi.on('bark',function(){
-  console.log('%s barked! energy:%s',this.getName(),this.getEnergy);
-})
+function onBark(){
+  console.log('%s barked! energy:%s',this.getName(),this.getEnergy());
+}
---
+++ 08-event/05-radio.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+
+var Radio = function(station){
+  var _listeners = {
+   // 'play':[fn1,fn2],
+   // 'stop':[];
+  };
+
+  
+  setTimeout(()=>{
+    emit('play',station);
+  },0);
+
+  setTimeout(()=>{
+    emit('stop',station);
+  },5000);
+
+  function emit(evt,arg){
+    if(typeof(_listeners[evt])=== 'undefined'){
+      console.error('Error:%s not defined',evt);
+      process.exit(1);
+    }
+
+    _listeners[evt].forEach(fn) =>{
+      fn.call(this,arg);
+    };
+  }
+
+  this.on = (evt,fn) =>{
+    if(typeof(_listeners[evt]) === 'undefined'){
+      _listeners[evt] = [];
+    }
+
+    _listeners[evt].push(fn);
+  }
+}
+
+module.exports = Radio;

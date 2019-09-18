adedd09b4db3e1d5faabb7281f9ea7dea8297f07
zhangshuo <443494891@qq.com>
2019-09-18 21:17:55 +0800

    add

--- 06-buffer/03-data-uri.js
+++ 06-buffer/03-data-uri.js
@@ -2,10 +2,26 @@
 
 const fs = require('fs'),
       log = console.log,
-      http = require('http');
+      http = require('http'),
+      path = require('path'),
+      file = process.argv[2];
 
-var data = fs.readFileSync('./qr-code.jpg').toString('base64')
+if(process.argv.length !== 3){
+  console.error('命令行参数格式：cmd fileName');
+  process.exit(1);
+}
+
+try{
+  
+  var data = fs.readFileSync(file).toString('base64');
+}catch(e){
+  console.error(e.message);
+  process.exit(2);
+}
+
 //log(data);
+var ext = path.extname(file);
+var uriData = 'data:image/' + ext.slice(1,ext.length)+';base64,'+data;
 
 var html = ''
   + '<!DOCTYPE html>'
@@ -15,11 +31,13 @@ var html = ''
     + '<title></title>'
   + '</head>'
   + '<body>'
-    + '<img src="data:./qr-code.jpg;base64,'+data+'">'
-  + '</body>'
-  + '</html>';
+    + '<img alt="'
+    +path.basename(file,ext)
+  +'"src="'+uriData+'"></body></html>';
 
 http.createServer((req,res) => {
+  log(req.headers);
+  log(req.url + '\n');
   res.end(html);
 }).listen(8080);
 
--- 06-buffer/04-bmp-info.js
+++ 06-buffer/04-bmp-info.js
@@ -1,10 +1,21 @@
 #!/usr/bin/node
 
 const fs = require('fs'),
-      log = console.log;
-
-var buf = fs.readFileSync('./nodejs-logo.bmp');
+      log = console.log,
+      file = process.argv[2];
+if(process.argv.length !== 3){
+  console.error('命令行参数格式：cmd fileName');
+  process.exit(1);
+}
+try{
+  var buf = fs.readFileSync(file);
+}catch(e){
+  console.error(e.message);
+  process.exit(2);
+}
+if(buf.toString('ascii',0,2) === 'BM'){
 
 log('width:',buf.readInt32LE(0x12));
 log('height:',buf.readInt32LE(0x16));
 log('deepth:',buf.readUInt16LE(0x1c));
+}

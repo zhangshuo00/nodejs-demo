a6f237282b85f10caa23c24268e44c5e14658de6
zhangshuo <443494891@qq.com>
2019-10-07 20:58:01 +0800

    add

--- task/02-task-one.js
+++ task/02-task-one.js
@@ -1,24 +1,40 @@
-#!/usr/bin/node
+t fs = require('fs'),
+          args = process.argv[2],
+                  dir = process.argv[3];
+try{
+  switch(args){
+            case 'list':
+                          var files = fs.readdirSync(__dirname),
+                                              i = 0;
+                                      var array = [];
+                                      for(i;i<files.length;i++){
+                                                        array[i] = new Object();
+                                                                        array[i].fileName = files[i];
+                                                                                        array[i].fileSize = fs.statSync('./'+files[i].size);
+                                                                                                    
+                                      }
+                                                  const json = JSON.stringify(array,null,2);
+                                                              console.log(json);
+                                                                          break;
+                                                                                  case 'mkdir':
+                                                                          fs.mkdir(dir,function(err){
+                                                                            if(err){
+                                                                                                  console.error(err.message);
+                                                                                                                      process.exit(1);
+                                                                                                                                      
+                                                                            }
+                                                                                        
+                                                                          });
+                                                                                      break;
+                                                                                              default:
+                                                                                                  console.error('请输入正确的参数');
+                                                                                                              process.exit(1);
+                                                                                                                          break;
+                                                                                                                              
+  }
 
-const fs = require('fs'),
-      args = process.argv[2],
-      dir = __dirname;
+}catch(err){
+      console.error('请输入正确的参数');
+          process.exit(1);
 
-var file1 = new Object();
-
-switch(args){
-  case 'ls':
-    fs.readdir(dir,function(err,files){
-      if(err){
-        console.error(err);
-      }
-      console.log(files[0]);
-    })
-    break;
-
-  case 'mkdir':
-    break;
-
-  default:
-    break;
 }
--- task/02-task.js
+++ task/02-task.js
@@ -3,34 +3,23 @@
 const fs = require('fs'),
       args = process.argv[2],
       dir = process.argv[3];
-<<<<<<< HEAD
-var arr = new Array();
-=======
-
->>>>>>> 471f9a5998d801ff4c8787d1d424662ad68862e8
 try{
   switch(args){
 
-    case 'ls':
+    case 'list':
       var files = fs.readdirSync(__dirname),
             i =0;
-<<<<<<< HEAD
       var array = [];
       for(i;i<files.length;i++){
         array[i] = new Object();
         array[i].fileName = files[i];
         array[i].fileSize = fs.statSync('./'+files[i]).size;
-       // const json = JSON.stringify(array[i]);
+        //const json = JSON.stringify(array[i],null,4);
         //console.log(json);
       }
 
-     const json = JSON.stringify(array);
+      const json = JSON.stringify(array,null,2);
       console.log(json);
-=======
-      for(i;i<files.length;i++){
-        console.log('fileName: '+ files[i] + ', fileSize: ' + fs.statSync('./'+files[i]).size);
-      }
->>>>>>> 471f9a5998d801ff4c8787d1d424662ad68862e8
       break;
     case 'mkdir':
       fs.mkdir(dir,function(err){

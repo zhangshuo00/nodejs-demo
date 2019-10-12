e10672f59d47e410d0e36bfd34e4deb0661a2339
zhangshuo <443494891@qq.com>
2019-10-12 16:03:21 +0800

    add child_process

--- 12-child-process/01-exec-file.js
+++ 12-child-process/01-exec-file.js
@@ -6,12 +6,19 @@ const cp = require('child_process');
 console.log('i am father process. pid:',process.pid);
 
 var cmd = '';
+var i =0;
+if(process.argv.length == 2){
+  cp.exec('cat 01-exec-file.js',(err,stdout)=>{
+    if(err) console.error(err);
+    console.log(stdout);
+  })
+}else{
+  for(i =2;i<process.argv.length;i++){
+    cmd += (process.argv[i] + ' ');
+  }
 
-for(var i =2;i<process.argv.length;i++){
-  cmd += (process.argv[i] + ' ');
+  cp.exec(cmd,(err,stdout)=>{
+    if(err) console.error(err);
+    console.log(stdout);
+  })
 }
-
-cp.execFile(cmd,(err,stdout)=>{
-  if(err) console.log(err);
-  console.log(stdout);
-})

85f1ba7b0c4b7992af7aa96c14aa8afc2cab2471
zhangshuo <443494891@qq.com>
2019-10-15 19:11:46 +0800

    add http

--- 16-http-server/04-response.js
+++ 16-http-server/04-response.js
@@ -3,25 +3,22 @@ const http = require('http'),
       log = console.log;
 
 http.createServer((req,res)=>{
-  log(`${req.method} ${req.url} HTTP/${req.httpVersion}`);
-  log(req.headers);
+     
+    var html = '<!DOCTYPE html><html><head><title>Hello</title><head><body><h1>Hello world!</h1></body></html>'; 
 
-  log();
-  req.pipe(process.stdout);
-  
-  var html = ''; 
+    if(req.url === '/'){
+      //200 ok
+      res.writeHead(200,{
+          'Content-Type' : 'text/html',
+          'Content-Length' : Buffer.byteLength(html,'utf-8')
+      });
+      res.end(html);
+    }else{
+      //404 err
+        res.statusCode = 404;
+        res.setHeader('Content-Type','text/plain');
 
-  if(req.url === '/'){
-    //200 ok
-    res.writeHead(200,{'Content-Type':'text/html','Content-Length':Buffer.byteLength(html,'utf-8')});
-    res.end(html);
-  }else{
-    //404 err
-    res.statusCode = 404;
-    res.setHeader('Content-Type','text/plain');
+        res.end('Resource not found!');
+    }
 
-    res.end('error');
-  }
-
-  res.end('hello world');
 }).listen(8080);

3df70cccbbd40cd9cfc2e2f5e03b6cc152c508d2
zhangshuo <443494891@qq.com>
2019-10-19 11:40:16 +0800

    add express

---
+++ task/blog/js/mine.js
@@ -0,0 +1,9 @@
+exports.type = {
+    'css':'text/css',
+    'html':'text/html',
+    'jpeg':'image/jpeg',
+    'jpg':'image/jpeg',
+    'png':'image/png',
+    'js':'text/javascript',
+    'json':'application/json'
+}
--- task/blog/server.js
+++ task/blog/server.js
@@ -2,24 +2,34 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const url = require('url');
+const mine = require('./js/mine').type;
 
 http.createServer((req,res)=>{
-    console.log(req.url);
-    res.setHeader('Content-Type','text/html;charset=utf-8');
-    if(req.url === '/login'){
-        fs.readFile(path.join(__dirname,'login.html'),(err,data)=>{
-            if(err){console.log(err)}
-            res.end(data);
-        })
+    // console.log(req.url);
+    var pathname = url.parse(req.url).pathname;
+    var pathnameArr = pathname.split('/');//将url中的pathname以/划分，返回数组
+
+    switch(pathnameArr[1]){
+        case 'login':
+        case 'list':
+        case 'addChapter':
+        case 'chapter':
+        case 'chapterList':
+            res.setHeader('content-type','text/html');
+            fs.readFile(path.join(__dirname,pathnameArr[1]+'.html'),(err,data)=>{
+                if(err) console.log(err);
+                res.end(data);
+            });
+            break;
+        
     }
-
-    // req.on('data',(data)=>{
-
-    // })
-    // req.on('end',()=>{
-    //     res.writeHead(200,{
-    //         'Content-Type':'text/plain',
-    //         'Access-Control-Allow-Origin':'*'
+    // if(req.url === '/login'){
+    //     res.setHeader('Content-Type','text/html;charset=utf-8');
+    //     fs.readFile(path.join(__dirname,'login.html'),(err,data)=>{
+    //         if(err){console.log(err)}
+    //         res.end(data);
     //     })
-    // })
+    // }
+    
 }).listen(8083);

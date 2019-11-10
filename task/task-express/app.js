2ba5a1f70e57aa4cf40ea328c37e2aa3ae0028be
zhangshuo <443494891@qq.com>
2019-11-10 11:10:41 +0800

    add task-express

--- task/task-express/app.js
+++ task/task-express/app.js
@@ -4,22 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var debug = require('debug')('my-application');
 
 
 var indexRouter = require('./routes/index');
-// var listRouter = require('./routes/list');
+var listRouter = require('./routes/list');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-// app.set('post',process.env.PORT || 3000);
-
-// var server = app.listen(app.get('port'),function(){
-//   debug('Express server listening on port' + server.address().port);
-// });
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -30,7 +24,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.use('/', indexRouter);
-// app.use('/list', listRouter);
+app.use('/list', listRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- task/task-express/routes/index.js
+++ task/task-express/routes/index.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var app = express();
-// var path = require('path');
-// var router = express.Router();
+var path = require('path');
+var fs = require('fs');
 
 /* GET home page. */
 app.get('/', function(req, res) {
     res.render('index');
 });
-app.post('/getList',function(req,res,next){
-    console.log(req.body.username,req.body.pwd)
+app.post('/list',function(req,res,next){
+    // console.log(req.body.username,req.body.pwd)
+    //读取data.json中的用户信息判断
+    var username = req.body.username;
+    var pwd = req.body.pwd;
+    var dataJSON = fs.readFileSync('./public/data.json','utf-8');
+    // console.log(JSON.parse(dataJSON).users);
+    var userInfo = JSON.parse(dataJSON).users;
+    for(var i = 0;i<userInfo.length;i++){
+        //判断成功后渲染list.ejs
+        if(userInfo[i].username === username && userInfo[i].password === pwd){
+            // console.log('登录成功');
+            res.render('list',{list : JSON.parse(dataJSON).chapterList});
+            break;
+        }else{
+            res.send('用户信息有误！');
+        }
+    }
+
+    //否则提示用户错误，登录失败
 })
 module.exports = app;
